c0fc1aef8763cfa1203e218edfae9d530df086c7
Tymek155 <tymek.maca@outlook.com>
2024-03-17 17:21:04 +0100

    :)

--- Main.java
+++ Main.java
@@ -2,75 +2,11 @@ import java.util.Arrays;
 
 public class Main {
     public static void main(String[] args){
-        //BubbleSort
-        int[] array = {5,4,3,2,1};
-        SortContext bubble = new SortContext(new BubbleSort());
-        bubble.execSort(array);
-        System.out.println("Posortowana tablica: "+ Arrays.toString(array));
-
-        int[] arrayb2 = {2,4,5,1,3};
-        bubble.execSort(arrayb2);
-        System.out.println("Posortowana tablica: "+ Arrays.toString(arrayb2));
-
-        int[] arrayb3 = {1,2,3,4,5};
-        bubble.execSort(arrayb3);
-        System.out.println("Posortowana tablica: "+ Arrays.toString(arrayb3));
-        System.out.println();
-
-        //CocktailSort
-        int[] arrayc1 = {5,4,3,2,1};
-        SortContext cocktail = new SortContext(new CocktailSort());
-        cocktail.execSort(arrayc1);
-        System.out.println("Posortowana tablica: "+ Arrays.toString(arrayc1));
-
-        int[] arrayc2 = {2,4,5,1,3};
-        cocktail.execSort(arrayc2);
-        System.out.println("Posortowana tablica: "+ Arrays.toString(arrayc2));
-
-        int[] arrayc3 = {1,2,3,4,5};
-        cocktail.execSort(arrayc3);
-        System.out.println("Posortowana tablica: "+ Arrays.toString(arrayc3));
-        System.out.println();
-
-        //HeapSort - tutaj tylko roznica czasowa dla ilosci elementow
-        int[] arrayh1 = {3,2,5,4,1};
-        SortContext heap = new SortContext(new HeapSort());
-        heap.execSort(arrayh1);
-        System.out.println("Posortowana tablica: "+ Arrays.toString(arrayh1));
-
-        int[] arrayh2 = {5,8,9,10,12,3,2,1,4,6,5,22};
-        heap.execSort(arrayh2);
-        System.out.println("Posortowana tablica: "+ Arrays.toString(arrayh2));
-        System.out.println();
-
-        //InsertSort
-        int[] arrayi1 = {5,4,3,2,1};
-        SortContext insert = new SortContext(new InsertionSort());
-        insert.execSort(arrayi1);
-        System.out.println("Posortowana tablica: "+ Arrays.toString(arrayi1));
-
-        int[] arrayi2 = {4,2,3,1,5};
-        insert.execSort(arrayi2);
-        System.out.println("Posortowana tablica: "+ Arrays.toString(arrayi2));
-
-        int[] arrayi3 = {1,2,3,4,5};
-        insert.execSort(arrayi3);
-        System.out.println("Posortowana tablica: "+ Arrays.toString(arrayi3));
-        System.out.println();
-
-        //SelectionSort
-        int[] arrays1 = {5,4,3,2,1};
-        SortContext selection = new SortContext(new SelectionSort());
-        selection.execSort(arrays1);
-        System.out.println("Posortowana tablica: "+ Arrays.toString(arrays1));
-
-        int[] arrays2 = {4,2,3,1,5};
-        selection.execSort(arrays2);
-        System.out.println("Posortowana tablica: "+ Arrays.toString(arrays2));
-
-        int[] arrays3 = {1,2,3,4,5};
-        selection.execSort(arrays3);
-        System.out.println("Posortowana tablica: "+ Arrays.toString(arrays3));
-
+        SortTest test = new SortTest();
+        test.testBubbleSort();
+        test.testCocktailSort();
+        test.testHeapSort();
+        test.testInsertionSort();
+        test.testSelectionSort();
     }
 }
--- SortTest.java
+++ SortTest.java
@@ -1,50 +1,83 @@
-import org.junit.jupiter.api.Test;
-
-import static org.junit.jupiter.api.Assertions.*;
+import java.util.Arrays;
 
 class SortTest {
-    @Test
+
     public void testBubbleSort(){
-        int[] array = {3,5,6,1,5,2,4};
-        int[] exp ={1,2,3,4,5,6};
-        Strategy bubbleSort = new BubbleSort();
-        bubbleSort.sort(array);
-        assertArrayEquals(array, exp);
+        int[] array = {5,4,3,2,1};
+        SortContext bubble = new SortContext(new BubbleSort());
+        bubble.execSort(array);
+        System.out.println("Posortowana tablica: "+ Arrays.toString(array));
+
+        int[] arrayb2 = {2,4,5,1,3};
+        bubble.execSort(arrayb2);
+        System.out.println("Posortowana tablica: "+ Arrays.toString(arrayb2));
+
+        int[] arrayb3 = {1,2,3,4,5};
+        bubble.execSort(arrayb3);
+        System.out.println("Posortowana tablica: "+ Arrays.toString(arrayb3));
+        System.out.println();
+
     }
 
-    @Test
     public void testCocktailSort(){
-        int[] array = {3,5,6,1,5,2,4};
-        int[] exp ={1,2,3,4,5,6};
-        Strategy cocktailSort = new CocktailSort();
-        cocktailSort.sort(array);
-        assertArrayEquals(array, exp);
+        int[] arrayc1 = {5,4,3,2,1};
+        SortContext cocktail = new SortContext(new CocktailSort());
+        cocktail.execSort(arrayc1);
+        System.out.println("Posortowana tablica: "+ Arrays.toString(arrayc1));
+
+        int[] arrayc2 = {2,4,5,1,3};
+        cocktail.execSort(arrayc2);
+        System.out.println("Posortowana tablica: "+ Arrays.toString(arrayc2));
+
+        int[] arrayc3 = {1,2,3,4,5};
+        cocktail.execSort(arrayc3);
+        System.out.println("Posortowana tablica: "+ Arrays.toString(arrayc3));
+        System.out.println();
     }
 
-    @Test
     public void testHeapSort(){
-        int[] array = {3,5,6,1,5,2,4};
-        int[] exp ={1,2,3,4,5,6};
-        Strategy heapSort = new HeapSort();
-        heapSort.sort(array);
-        assertArrayEquals(array, exp);
+        int[] arrayh1 = {3,2,5,4,1};
+        SortContext heap = new SortContext(new HeapSort());
+        heap.execSort(arrayh1);
+        System.out.println("Posortowana tablica: "+ Arrays.toString(arrayh1));
+
+        int[] arrayh2 = {5,8,9,10,12,3,2,1,4,6,5,22};
+        heap.execSort(arrayh2);
+        System.out.println("Posortowana tablica: "+ Arrays.toString(arrayh2));
+        System.out.println();
+
     }
 
-    @Test
     public void testInsertionSort(){
-        int[] array = {3,5,6,1,5,2,4};
-        int[] exp ={1,2,3,4,5,6};
-        Strategy insertSort = new InsertionSort();
-        insertSort.sort(array);
-        assertArrayEquals(array, exp);
+        int[] arrayi1 = {5,4,3,2,1};
+        SortContext insert = new SortContext(new InsertionSort());
+        insert.execSort(arrayi1);
+        System.out.println("Posortowana tablica: "+ Arrays.toString(arrayi1));
+
+        int[] arrayi2 = {4,2,3,1,5};
+        insert.execSort(arrayi2);
+        System.out.println("Posortowana tablica: "+ Arrays.toString(arrayi2));
+
+        int[] arrayi3 = {1,2,3,4,5};
+        insert.execSort(arrayi3);
+        System.out.println("Posortowana tablica: "+ Arrays.toString(arrayi3));
+        System.out.println();
+
     }
 
-    @Test
     public void testSelectionSort(){
-        int[] array = {3,5,6,1,5,2,4};
-        int[] exp ={1,2,3,4,5,6};
-        Strategy selectionSort = new SelectionSort();
-        selectionSort.sort(array);
-        assertArrayEquals(array, exp);
+        int[] arrays1 = {5,4,3,2,1};
+        SortContext selection = new SortContext(new SelectionSort());
+        selection.execSort(arrays1);
+        System.out.println("Posortowana tablica: "+ Arrays.toString(arrays1));
+
+        int[] arrays2 = {4,2,3,1,5};
+        selection.execSort(arrays2);
+        System.out.println("Posortowana tablica: "+ Arrays.toString(arrays2));
+
+        int[] arrays3 = {1,2,3,4,5};
+        selection.execSort(arrays3);
+        System.out.println("Posortowana tablica: "+ Arrays.toString(arrays3));
+
     }
 }
